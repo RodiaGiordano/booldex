f0000bce6fd9e96c01ea260b6074471a5a1bd78c
RodiaGiordano <rodiagiordano93@gmail.com>
2024-03-11 14:07:53 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="pokedex"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 50px;
+
+  .pokedex {
+    border: 10px solid #8b0001;
+    border-radius: 40px;
+
+    min-height: 80vh;
+    min-width: 80vw;
+  }
+}
+</style>
